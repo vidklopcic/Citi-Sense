8fbe2a892fd0a9e818a2bf1ac0147da05d15051c
vidklopcic <klopcic.vid@gmail.com>
2015-06-13 18:21:37 +0200

    added favorite place popup

--- app/src/main/java/eu/citi_sense/vic/citi_sense/support_classes/sliding_up_pane/SlidingUpPaneExpandedFragment.java
+++ app/src/main/java/eu/citi_sense/vic/citi_sense/support_classes/sliding_up_pane/SlidingUpPaneExpandedFragment.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -24,7 +30,8 @@ public class SlidingUpPaneExpandedFragment extends Fragment {
     private ArrayList<FavoritePlace> mFavoritePlaces;
     private Activity mActivity;
     private PlaceClickListener mPlaceClickListener;
-
+    private RelativeLayout mEditPlaceLayout;
+    private Integer mEditItemPosition;
     public interface PlaceClickListener {
         void onClick(FavoritePlace place);
     }
@@ -42,9 +49,10 @@ public class SlidingUpPaneExpandedFragment extends Fragment {
         if (mFragmentView != null) {
             return;
         }
-        LayoutInflater layoutInflater = (LayoutInflater) mActivity.getApplicationContext()
+        LayoutInflater inflater = (LayoutInflater) mActivity.getApplicationContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        mFragmentView = (RelativeLayout) layoutInflater.inflate(R.layout.expanded_sliding_pane_fragmnet, null);
+        mFragmentView = (RelativeLayout) inflater.inflate(R.layout.expanded_sliding_pane_fragmnet, null);
+        mEditPlaceLayout = (RelativeLayout) inflater.inflate(R.layout.edit_favorite_place_popup, null);
         mFavoritePlaces = new ArrayList<>(FavoritePlace.listAll(FavoritePlace.class));
         mFavoritePlacesListView = (ListView) mFragmentView.findViewById(R.id.favorite_places_list_view);
         mFavoritePlacesAdapter = new FavoritePlacesAdapter(mActivity.getApplicationContext(), mFavoritePlaces);
@@ -52,6 +60,33 @@ public class SlidingUpPaneExpandedFragment extends Fragment {
         mFavoritePlacesAdapter.setOnClickListener(new FavoritePlacesAdapter.PlaceClickListener() {
             @Override
             public void onEditClick(View view) {
+                mEditItemPosition = (Integer) view.getTag();
+                final PopupWindow popupWindow = new PopupWindow(mEditPlaceLayout, LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT, true);
+                popupWindow.showAtLocation(mFragmentView, Gravity.CENTER, 0, 0);
+                Button removeButton = (Button) mEditPlaceLayout.findViewById(R.id.place_remove_btn);
+                Button saveButton = (Button) mEditPlaceLayout.findViewById(R.id.place_save_btn);
+                EditText name = (EditText) mEditPlaceLayout.findViewById(R.id.place_name_edit);
+                LinearLayout wrapper = (LinearLayout) mEditPlaceLayout.findViewById(R.id.place_popup_wrapper);
+                removeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        popupWindow.dismiss();
+                    }
+                });
+                saveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        popupWindow.dismiss();
+                    }
+                });
+                wrapper.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        popupWindow.dismiss();
+                    }
+                });
+
 
             }
 
